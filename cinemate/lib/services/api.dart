9e6eff33b346e0ec6427d87d875e90c516a150c9
Furkan TURAN <turan.cmd@gmail.com>
2024-07-20 21:41:14 +0300

    chatbot için güncellemeler

--- cinemate/lib/services/api.dart
+++
@@ -1,5 +0,0 @@
-String geminiApi = 'AIzaSyC7VRQaE7LuqWwuL7mmrmKHBxTv2lFxY2c';
-const String searchApi = 'f09947e5d5bbc3a4ba0a6e149efb63f9';
-const String favoriteApi = 'f09947e5d5bbc3a4ba0a6e149efb63f9';
-const String homeApi = 'f09947e5d5bbc3a4ba0a6e149efb63f9';
-const String profileApi = 'f09947e5d5bbc3a4ba0a6e149efb63f9';
